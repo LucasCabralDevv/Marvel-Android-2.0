48a953108b6f0f94829c772077606203ac1bb663
lucas <lucascabraldevv@gmail.com>
2022-05-17 00:38:17 -0300

    init commit

---
+++ app/src/main/java/com/lucascabral/android_marvel_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lucascabral.android_marvel_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
